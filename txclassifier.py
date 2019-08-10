837fc382d4c6695613a1bb30496f7034ff65390b
Fabian Varisco <fabianvarisco@gmail.com>
2019-08-10 14:32:10 -0300

    agrega obj json en state y changes para facilitar el acceso a los atributos

--- txclassifier.py
+++ txclassifier.py
@@ -51,56 +51,48 @@ DICT_IMP_ORG = {
   5902: 902
 }
 
-EMPTY_DATAFRAME = pd.DataFrame()
+EMPTY_WSETDF = pd.DataFrame(columns=['block', 'txseq', 'item', 'key', 'value', 'isdelete', 'orgs', 'obj'])
 
 def mk_resolver_orgs( state: pd.DataFrame ):
 
     # Inner func using state
-    def resolve_orgs_from_dors( dom_value: str ):
+    def resolve_orgs_from_dors( dom: dict ) -> str:
+        if len( state ) == 0: return None
+        
         # TODO: revisar con team SR
         # hay que recuperar las disintas orgs desde los dor del state
-      
-        if len( state ) == 0: return None
-      
-        # busca roles del domicilio y se queda con los values
-        roles = state.loc[ state.component_type == "dor", "value" ]
-      
-        if len( roles ) == 0: return None
-      
-        dom = json.loads( dom_value )
-      
+
+        roles = state.loc[ state["component_type"] == "dor", "obj" ].values
+        
         orgs = set()
-      
-        for v in roles.values:
-            dor = json.loads( v )
-            if dor["tipo"] == dom["tipo"] and dor["orden"] == dom["orden"]:
-               if dor["org"] not in orgs: 
-                  if dor["org"] > 1:
-                     # TODO: no debe ser 1
-                     orgs.add( dor["org"] )
+
+        for rol in roles:
+            if  rol.get( "org", -1 ) > 1 \
+            and rol.get( "org" ) not in orgs \
+            and rol.get( "tipo", -1 ) == dom.get( "tipo", -2 ) \
+            and rol.get( "orden", -1 ) == dom.get( "orden", -2 ):
+                orgs.add( rol.get( "org" ) )
         
         return None if len( orgs ) == 0 else ",".join( orgs )
     
     # Inner func
     # Retorna un string conteniendo una lista de orgs 
     # separados por coma (roles de distintas orgs)
-    def resolve_orgs( component_type: str, value: str ):
+    def resolve_orgs( component_type: str, obj: dict ) -> str:
     
         if component_type == 'cms': return "900"
       
         if component_type in [ 'imp', 'con' ]: 
-           imp = json.loads( value )["impuesto"]
-           org = DICT_IMP_ORG.get( imp, None )
+           org = DICT_IMP_ORG.get( obj.get( "impuesto", -1 ), None )
            return None if org == None else str( org )
       
         if component_type in [ 'jur', 'act', 'dom', 'dor' ]: 
-           org = json.loads( value )["org"]
-           if org > 1: return str( org )
+           if obj.get( "org", -1 ) > 1: return str( obj.get( "org" ) )
       
-        if len( state ) == 0: return None
+        if len( state ) == 0 or component_type != 'dom': return None
       
         # dom de AFIP: en el state puede tener roles de distintas jurisdicciones
-        return None if component_type != 'dom' else resolve_orgs_from_dors( value )
+        return resolve_orgs_from_dors( obj )
     
     # return clousure func 
     return resolve_orgs
@@ -110,89 +102,86 @@ def mk_resolver_orgs( state: pd.DataFrame ):
 # Try using .loc[row_indexer,col_indexer] = value instead
 pd.options.mode.chained_assignment = None
 
-def add_org( df: pd.DataFrame, state: pd.DataFrame ):
+def add_orgs_column( df: pd.DataFrame, state: pd.DataFrame ) -> pd.DataFrame:
+    if len( df ) == 0: return df
+    # state no se puede pasar como argumento al resolve_orgs
+    # entonces se utiliza una variable global
+    resolve_orgs = mk_resolver_orgs( state )    
+    df["orgs"] = df.apply(lambda row: resolve_orgs( row.component_type, row.obj ), axis=1 ) 
+    return df
 
-  if len( df ) == 0: return df
+def mk_wsetdf( res ) -> pd.DataFrame:
+    if len( res ) == 0: return EMPTY_WSETDF
+    df = pd.DataFrame(res, columns=['block', 'txseq', 'item', 'key', 'value', 'isdelete']) 
+    new = df["key"].str.split("#", n=1, expand=True)
+    personaid = new[0].str.split(":", n=1, expand=True)
+    component = new[1].str.split(":", n=1, expand=True)
+    df["personaid"]      = personaid[1].astype( int )
+    df["component_type"] = component[0].astype( str ) # ej: dom
+    df["component_key"]  = component[1].astype( str ) # ej: 1.3.10, vamos a tratar de no usuar la key . mejor procesar el value json
+    return df
 
-  # no se puede pasar como argumento al resolve_orgs
-  # entonces se utiliza una variable global
-  resolve_orgs = mk_resolver_orgs( state )
-
-  df['org'] = df.apply(lambda row: resolve_orgs( row.component_type, 
-                                                 row.value ), 
-                                                 axis=1 ) 
-  return df
-
-def mk_wsetdf( res ):
-  df = pd.DataFrame(res, columns=['block', 'txseq', 'item', 'key', 'value', 'isdelete']) 
-  new = df["key"].str.split("#", n=1, expand=True)
-  personaid = new[0].str.split(":", n=1, expand=True)
-  component = new[1].str.split(":", n=1, expand=True)
-  df["personaid"]      = personaid[1]
-  df["component_type"] = component[0] # ej: dom
-  df["component_key"]  = component[1] # ej: 1.3.10, vamos a tratar de no usuar la key . mejor procesar el value json
-  return df
+def add_obj_column( df: pd.DataFrame ) -> pd.DataFrame:
+    if len( df ) == 0: return df
+    df["obj"] = df.apply(lambda row: None if row.value is None or not isinstance( row.value, str ) else json.loads( row.value ), axis=1 )
+    return df
 
 def get_persona_state( block: int, personaid: int ):
-  keypattern = "per:" + personaid + "#%"
-  res = db.queryall( QUERY_WSET_BY_KEYPATTERN, { "block" : block, "keypattern" : keypattern } )
-  if len(res) == 0: return EMPTY_DATAFRAME
-  
-  state = mk_wsetdf( res ).groupby( ['key'] ).first()
-  # TODO: eliminar los filas con delete antes de retornar el state
-  return state
+    keypattern = "per:" + str( personaid ) + "#%"
+    res = db.queryall( QUERY_WSET_BY_KEYPATTERN, { "block" : block, "keypattern" : keypattern } )
+    if len(res) == 0: return EMPTY_WSETDF  
+    state = mk_wsetdf( res ).groupby( ['key'] ).first()
+    return add_orgs_column( add_obj_column( state[ state["isdelete"] != "T" ] ), EMPTY_WSETDF )
 
-def process_txpersona( block: int, txseq: int, personaid: int, changes: pd.DataFrame ):
-  # print( "processing block {} personaid {} ...".format( block, personaid ) )
+def process_txpersona( block: int, txseq: int, personaid: int, changes: pd.DataFrame ) -> pd.DataFrame:
+    # print( "processing block {} personaid {} ...".format( block, personaid ) )
 
-  state = get_persona_state( block, personaid )
+    state = get_persona_state( block, personaid )
 
-  if len( state ) > 0: 
-     state = add_org( state, EMPTY_DATAFRAME )
-     if state.org.count() > 0:
-        print("personaid {} con state con datos jurisdiccionales ...".format( personaid ))
-     else:
-        # Si el state no tiene datos jurisdiccionales se vacia
-        state = state[0:0] 
+    if len( state ) > 0:
+       if state["orgs"].count() == 0:
+          # Si el state no tiene datos jurisdiccionales se vacia
+          state = EMPTY_WSETDF 
 
-  changes = add_org( changes, state )
+    changes = add_orgs_column( add_obj_column( changes ), state )
 
-  if changes.org.count() == 0: return None # sin transacciones jurisdiccionales
+    txs = pd.DataFrame(columns=['org', 'tx'])
 
-  print("personaid {} con datos jurisdiccionales !!!".format( personaid ))
-  print( changes[["key","org"]] ) 
+    if state["orgs"].count() == 0 and changes["orgs"].count() == 0: 
+       return txs # sin transacciones jurisdiccionales
 
-  txs = pd.DataFrame(columns=['org', 'tx'])
+    print( "personaid {} con datos jurisdiccionales !!!".format( personaid ))
+    print( "state with orgs: {}".format( state["orgs"].count() ) )
+    print( state.loc[getattr(state, "orgs").notnull(), "orgs" ] )
+    print( "changes with orgs: {}".format( changes["orgs"].count() ) )
+    print( changes.loc[getattr(changes, "orgs").notnull(), "orgs"] )
 
-  # TODO: Tener en cuenta que las rows puede ser DELETEs !!!!
-  #
-  # {"impuesto":11,"inscripcion":"2019-05-31","estado":"AC","dia":1,"periodo":201905,"motivo":{"id":44},"ds":"2019-05-31"}
-  #
-  # MIGRACION
-  # - no tiene del
-  # - tiene imp con org X y estado AC 
-  # - no tiene state con org X
-  # - tiene dom con org == component_key[0] # Domicilio migrado
-  # accion:
-  # - una tx { imp.org, MIGRACION }
-  # - una tx por cada jur { jur.org, MIGRACION_DESDE_CM }
-  #
+    # TODO: Tener en cuenta que las rows puede ser DELETEs !!!!
+    #
+    # {"impuesto":11,"inscripcion":"2019-05-31","estado":"AC","dia":1,"periodo":201905,"motivo":{"id":44},"ds":"2019-05-31"}
+    #
+    # MIGRACION
+    # - no tiene del
+    # - tiene imp con org X y estado AC 
+    # - no tiene state con org X
+    # - tiene dom con org == component_key[0] # Domicilio migrado
+    # accion:
+    # - una tx { imp.org, MIGRACION }
+    # - una tx por cada jur { jur.org, MIGRACION_DESDE_CM }
+    #
 
-  # ALTA EN JUR
-  # - tiene imp con org X y estado AC 
-  # - no tiene state con org X
-  # - no tiene dom con org == component_key[0] # Domicilio migrado
-  # accion:
-  # - una tx { imp.org, ALTA EN JUR }
-  # - una tx por cada jur { jur.org, ALTA EN CM }
-  #
+    changes_has_deletes = (changes["isdelete"].count() > 0)
 
-  # CM CAMBIO DE JURISDICCION
-  # - tiene jur con value distinto de state
-  #
-  #
-  #
-  #
+    # ALTA EN JUR
+    # - tiene imp con org X y estado AC 
+    # - no tiene state con org X
+    # - no tiene dom con org == component_key[0] # Domicilio migrado
+    # accion:
+    # - una tx { imp.org, ALTA EN JUR }
+    # - una tx por cada jur { jur.org, ALTA EN CM }
+    #
+    # CM CAMBIO DE JURISDICCION
+    # - tiene jur con value distinto de state
 
 USER = 'HLF'
 PASSW = 'HLF'
